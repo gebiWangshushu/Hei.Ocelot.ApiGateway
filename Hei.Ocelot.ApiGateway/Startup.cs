e1d6c215ce5fbd38c14b2a3f27f6ef7c96018acd
wangnima <wangsiheng@mail.39.net>
2020-08-04 19:18:59 +0800

    加入Administration、consul/Eureka/k8s,接入identityserver4;未测试完全

--- Hei.Ocelot.ApiGateway/Startup.cs
+++ Hei.Ocelot.ApiGateway/Startup.cs
@@ -9,7 +9,9 @@ using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
 using Ocelot.Provider.Eureka;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Hei.Ocelot.ApiGateway
 {
@@ -29,38 +31,83 @@ namespace Hei.Ocelot.ApiGateway
         }
 
         public void ConfigureServices(IServiceCollection services)
-        {            
+        {
+            //set Cors Policy
+            var corsOrigins = Configuration.GetSection("CorsOrigins")?.Get<string[]>();
+            if (corsOrigins?.Length > 0)
+            {
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(builder =>
+                    {
+                        builder.AllowAnyMethod().AllowAnyHeader().WithOrigins(corsOrigins);
+                    });
+                });
+            }
+
             var ocelot = services.AddOcelot(Configuration);
 
             //add Administration
-            var identityOptions = Configuration.GetSection("AddAdministration:IdentityServer")?.Get<IdentityServerAuthenticationOptions>();
-            ocelot.AddAdministration(Configuration.GetValue<string>("AddAdministration:Path")?? "/administration", options =>
+            if (string.IsNullOrEmpty(Configuration.GetValue<string>("Administration:Path"))==false)
             {
-                options.SupportedTokens = SupportedTokens.Both;
-                options.Authority = identityOptions.Authority;
-                options.ApiName = identityOptions.ApiName;
-                options.RequireHttpsMetadata = identityOptions.RequireHttpsMetadata;
-                options.ApiSecret = identityOptions.ApiSecret;
-            });
+                var identityOptions = Configuration.GetSection("Administration:IdentityServer")?.Get<IdentityServerAuthenticationOptions>();
+                ocelot.AddAdministration(Configuration.GetValue<string>("Administration:Path"), options =>
+                 {
+                     options.SupportedTokens = SupportedTokens.Both;
+                     options.Authority = identityOptions.Authority;
+                     options.ApiName = identityOptions.ApiName;
+                     options.RequireHttpsMetadata = identityOptions.RequireHttpsMetadata;
+                     options.ApiSecret = identityOptions.ApiSecret;
+                 });
+            }
 
+            //add ServiceDiscoveryProvider
             if ("Consul".Equals(Configuration["GlobalConfiguration:ServiceDiscoveryProvider:Type"],StringComparison.InvariantCultureIgnoreCase))
             {
                 ocelot.AddConsul();
             }
-
-            if ("Eureka".Equals(Configuration["GlobalConfiguration:ServiceDiscoveryProvider:Type"], StringComparison.InvariantCultureIgnoreCase))
+            else if ("Eureka".Equals(Configuration["GlobalConfiguration:ServiceDiscoveryProvider:Type"], StringComparison.InvariantCultureIgnoreCase))
             {
                 ocelot.AddEureka();
             }
-
-            if ("kube".Equals(Configuration["GlobalConfiguration:ServiceDiscoveryProvider:Type"], StringComparison.InvariantCultureIgnoreCase))
+            else if("Kube".Equals(Configuration["GlobalConfiguration:ServiceDiscoveryProvider:Type"], StringComparison.InvariantCultureIgnoreCase))
             {
                 ocelot.AddKubernetesFixed();
+            }
+
+            //add route Authentication
+            var idProviders = Configuration.GetSection("IdentityProvider")?.Get<List<IdentityServerAuthenticationOptions>>();
+            if (idProviders.Count > 0)
+            {
+                var authBuilder = services.AddAuthentication();
+                foreach (var p in idProviders)
+                {
+                    authBuilder.AddIdentityServerAuthentication(p.ApiName, options =>
+                    {
+                        options.Authority = p.Authority;
+                        options.ApiName = p.ApiName;
+                        options.RequireHttpsMetadata = p.RequireHttpsMetadata;
+                        options.SupportedTokens = SupportedTokens.Both;
+                        options.ApiSecret = p.ApiSecret;
+                    });
+                }
             }
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var corsOrigins = Configuration.GetSection("CorsOrigins")?.Get<string[]>();
+            if (corsOrigins?.Length > 0)
+            {
+                app.UseCors();
+            }
+
+            var idProviders = Configuration.GetSection("IdentityProvider")?.Get<List<IdentityServerAuthenticationOptions>>();
+            if (idProviders.Count > 0)
+            {
+                app.UseAuthentication();
+            }
+
             app.UseOcelot().Wait();
         }
     }
